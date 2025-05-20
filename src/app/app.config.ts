831308bbbef3ba91011a783c3071e0b19c5dd61d
Francois Botes <fbotes@nm.na>
2025-05-20 20:03:44 +0200

    enviroments

---
+++ src/app/app.config.ts
@@ -0,0 +1,26 @@
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { TimelineAllModule }    from '@syncfusion/ej2-angular-layouts';
+
+import { environment } from '../environments/environment';
+export const appConfig: ApplicationConfig = {
+  providers: [
+    importProvidersFrom(TimelineAllModule, MatDatepickerModule,
+      MatNativeDateModule),
+    
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp(environment.firebase)
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
+};
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+// src/environments/environment.ts
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'forceagri-46988',
+    appId: '1:526162217915:web:515eac63d241fab8f8529e',
+    storageBucket: 'forceagri-46988.firebasestorage.app',
+    apiKey: 'AIzaSyADIY57ZmasTmV8rLMNCsRWRellwhkug70',
+    authDomain: 'forceagri-46988.firebaseapp.com',
+    messagingSenderId: '526162217915',
+    measurementId: 'G-3JF7XPNCN3',
+  }
+};
